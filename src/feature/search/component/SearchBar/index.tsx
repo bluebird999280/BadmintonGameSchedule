98859bfbe61f754b542347d98c71fc3993a0011b
Psycoma <psycoma99@gmail.com>
2023-03-27 09:25:46 +0900

    @Design SearchBar 디자인 완성

--- src/feature/search/component/SearchBar/index.tsx
+++ src/feature/search/component/SearchBar/index.tsx
@@ -1,5 +1,18 @@
+import { useState, useCallback } from "react"
+import { Wrapper, StyledInput } from "./style"
+
 function SearchBar(): JSX.Element {
-  return <div></div>
+  const [value, setValue] = useState("")
+
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }, [])
+
+  return (
+    <Wrapper>
+      <StyledInput value={value} onChange={onChange} />
+    </Wrapper>
+  )
 }
 
 export default SearchBar
---
+++ src/feature/search/component/SearchBar/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  width: 500px;
+  height: 60px;
+  margin: 10px;
+  padding: 0 10px;
+  border-radius: 10px;
+  border: 1px solid #e9ecef;
+  position: relative;
+`
+
+export const StyledInput = styled.input`
+  width: 100%;
+  height: 100%;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  flex-grow: 0;
+  white-space: nowrap;
+  border: none;
+  outline: none;
+`
