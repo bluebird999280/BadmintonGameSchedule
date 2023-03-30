8e93e8bfece927606fcb66b6c72a37cbdafe0d14
Psycoma <psycoma99@gmail.com>
2023-03-30 16:33:04 +0900

    @Design TimeTable 컴포넌트 디자인 만들기 완성

--- src/feature/schedule/style.ts
+++ src/feature/schedule/style.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div``
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
