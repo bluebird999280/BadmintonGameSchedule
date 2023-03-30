5bac4262e874362d5bf01f3de945d02fcefc2c28
Psycoma <psycoma99@gmail.com>
2023-03-31 08:59:28 +0900

    @Rename SearchForm폴더를 common폴더로 이동

--- src/feature/schedule/style.ts
+++ src/feature/schedule/style.ts
@@ -1,7 +1,4 @@
 import styled from "styled-components"
 
 export const Wrapper = styled.div``
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`
+export const Container = styled.div``
