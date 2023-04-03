4982983245def3c73a54aaef62d5b22b53eba693
Psycoma <psycoma99@gmail.com>
2023-04-03 09:09:09 +0900

    @Rename search feature component 정리

---
+++ src/feature/search/component/PaginationList/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const ButtonWrapper = styled.div`
+  display: inline-flex;
+  border: 1px solid rgb(233, 236, 239);
+  border-radius: 5px;
+`
---
+++ src/feature/search/component/SearchForm/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Wrapper,
+  Container,
+  StyledInputWrapper,
+  StyledInput,
+  SearchImageWrapper,
+} from "./style"
+import { ISearchFormProps } from "./type"
+
+const searchImageUrl = process.env.PUBLIC_URL + "/icon/search.png"
+function SearchForm({
+  query,
+  onChange,
+  onSubmit,
+}: ISearchFormProps): JSX.Element {
+  return (
+    <Wrapper>
+      <Container onSubmit={onSubmit}>
+        <StyledInputWrapper>
+          <StyledInput value={query} onChange={onChange} />
+        </StyledInputWrapper>
+        <SearchImageWrapper type="submit">
+          <img src={searchImageUrl} />
+        </SearchImageWrapper>
+      </Container>
+    </Wrapper>
+  )
+}
+
+export default SearchForm
