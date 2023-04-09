de432985305bf1fe3e8e765a2da51cec9ce8eed9
Psycoma <psycoma99@gmail.com>
2023-04-10 07:52:08 +0900

    @Feat 화면 왼쪽에 현재 페이지와 관련한 제목 보여주기

--- src/page/layout/style.ts
+++ src/page/layout/style.ts
@@ -4,6 +4,18 @@ export const Wrapper = styled.div`
   padding: 20px;
 `
 
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .title {
+    font-size: 28px;
+    font-weight: bold;
+  }
+  margin-bottom: 10px;
+`
+
 export const BreadcrumbWrapper = styled.div`
-  margin-bottom: 30px;
+  margin-top: 10px;
 `
