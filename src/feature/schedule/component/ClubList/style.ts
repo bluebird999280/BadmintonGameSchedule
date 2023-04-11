a3c219c3817fa3c88e692b697955bae0f5d8963c
Psycoma <psycoma99@gmail.com>
2023-04-11 15:36:25 +0900

    @remove 쓰지 않는 schedule component 삭제

--- src/feature/schedule/component/ClubList/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const Wrapper = styled.div`
-  max-width: 500px;
-`
-export const Container = styled.div``
-export const Title = styled.div``
-export const ClubRow = styled.div``
